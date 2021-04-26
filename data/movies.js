c28e7ec00bd4dfd84e27bafa28f92f5d8b765662
canxivtp2511 <phodendopaypal@gmail.com>
2021-04-27 01:11:51 +0700

    update

---
+++ data/movies.js
@@ -0,0 +1,258 @@
+var movies = [
+  {
+    "name": 'PHO BO <br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Pho is a traditional Vietnamese dish, originating from Hanoi and Nam Dinh, and is considered one of the typical dishes for Vietnamese cuisine.",
+    "rating": 9.3,
+    "director": "$1.20",
+    "year": '<img src="img/instagram/phobo.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 10,
+    "genre": [
+      "BREAKFAST",
+      "BREAKFAST"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": 'BANH MI THIT <br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Vietnamese bread is a Vietnamese street food consisting of a crust of toast with crispy skin and soft intestines; inside is the multiplication part.",
+    "rating": 9.2,
+    "director": "$1.20",
+    "year": '<img src="img/instagram/banhmithit.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 5,
+    "genre": [
+      "BREAKFAST",
+      "BREAKFAST"
+    ],
+    "certificate": "R",
+    "date": "1974-07-12T10:29:29+05:30",
+    "actor": "Marlon Brando",
+    "id": 2
+  },
+  {
+    "name": 'BANH CUON <br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Roll cake also known as cake or wet cake is the name of a food made from steamed dough coated thinly, curled up, inside a vegetable or meat filling.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/banhcuon.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 5,
+    "genre": [
+      "BREAKFAST",
+      "BREAKFAST"
+    ],
+    "certificate": "R",
+    "date": "1992-08-23T10:29:29+05:30",
+    "actor": "Al Pacino",
+    "id": 3
+  },
+  {
+    "name": 'MI QUANG<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Mi Quang is usually made from fine ground rice flour mixed with flaxseed water and a golden star egg and then coated in thin layers, sliced horizontally to create a thin noodle about 5-10mm.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/miquang.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 11,
+    "genre": [
+      "BREAKFAST",
+      "BREAKFAST"
+    ],
+    "certificate": "R",
+    "date": "1994-02-09T10:29:29+05:30",
+    "actor": "John Travolta",
+    "id": 4
+  },
+  {
+    "name": 'COM TAM<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Com Tam is a Vietnamese dish made from rice with broken rice grains. Broken rice refers to broken rice grains, and rice refers to cooked rice.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/comtam.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 12,
+    "genre": [
+      "Adventure",
+      "Western"
+    ],
+    "certificate": "TV_14",
+    "date": "1980-10-05T10:29:29+05:30",
+    "actor": "Clint Eastwood",
+    "id": 5
+  },
+  {
+    "name": 'SUSHI<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Sushi is a Japanese dish of rice mixed with vinegar (shari) combined with other ingredients (neta).",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/shushi.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 15,
+    "genre": [
+      "Action",
+      "Crime",
+      "Drama",
+      "Thriller"
+    ],
+    "certificate": "PG_13",
+    "date": "2002-09-14T10:29:29+05:30",
+    "actor": "Christian Bale",
+    "id": 6
+  },
+  {
+    "name": 'COM CHIEN HAI SAN<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Fried rice is a favorite breakfast dish of many people. A simple way to make burnt rice, just rice, cooking oil and seasoning is enough.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/comchienhaisan.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 15,
+    "genre": [
+      "Drama"
+    ],
+    "certificate": "APPROVED",
+    "date": "1993-11-21T10:29:29+05:30",
+    "actor": "Henry Fonda",
+    "id": 7
+  },
+  {
+    "name": 'CANH CU SEN<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Just eating or drinking lotus root water will help you sleep better and deeper.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/canhcusen.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 15,
+    "genre": [
+      "Biography",
+      "Drama",
+      "History",
+      "War"
+    ],
+    "certificate": "R",
+    "date": "1991-03-02T10:29:29+05:30",
+    "actor": "Liam Neeson",
+    "id": 8
+  },
+  {
+    "name": 'GA SOT CAY<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Spicy chicken dish is easy to make, attractive when eaten with white rice thanks to the sour, spicy taste from ketchup and chili sauce.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/gasotcay.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 13,
+    "genre": [
+      "DINNER",
+      "DINNER"
+    ],
+    "certificate": "PG_13",
+    "date": "2007-01-29T10:29:29+05:30",
+    "actor": "Elijah Wood",
+    "id": 9
+  },
+  {
+    "name": 'BO BIT TET<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Beef steak is a famous European dish favored by Vietnamese people, I don't know when it was introduced to Vietnam, but now it's too popular.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/bobittet.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 14,
+    "genre": [
+      "DINNER"
+    ],
+    "certificate": "R",
+    "date": "1989-05-06T10:29:29+05:30",
+    "actor": "Brad Pitt",
+    "id": 10
+  },
+  {
+    "name": 'SPAGHETTI<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Spaghetti, more commonly known as pasta, is a long, small, cylindrical type of pasta. It is an essential food in traditional Italian cuisine.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/spaghetti.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 12,
+    "genre": [
+      "DINNER",
+      "DINNER",
+      "DINNER"
+    ],
+    "certificate": "PG",
+    "date": "1993-02-18T10:29:29+05:30",
+    "actor": "Mark Hamill",
+    "id": 11
+  },
+  {
+    "name": 'PIZZA<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
+    "outline": "Pizza, which Vietnamese people often read as, is a flat, round cake made from water, flour and yeast, after being incubated for at least 24 hours and molded into a round and flat cake. and is put in the oven to cook.",
+    "rating": 9.1,
+    "director": "$1.21",
+    "year": '<img src="img/instagram/pizza.jpg" style="height: 100%; width: 100%;">',
+    "stars": [
+      "Món mặn",
+      "Món sáng",
+      "chưa biết"
+    ],
+    "runtime": 8,
+    "genre": [
+      "DINNER",
+      "DINNER",
+      "Indiafood"
+    ],
+    "certificate": "PG",
+    "date": "1993-02-18T10:29:29+05:30",
+    "actor": "Mark Hamill",
+    "id": 12
+  }
+]
