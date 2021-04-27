a4737dfac38f679f81299743570825f629c9de2c
canxivtp2511 <phodendopaypal@gmail.com>
2021-04-27 09:25:02 +0700

    Update 9h

--- data/movies.js
+++ data/movies.js
@@ -24,7 +24,7 @@ var movies = [
     "name": 'BANH MI THIT <br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Vietnamese bread is a Vietnamese street food consisting of a crust of toast with crispy skin and soft intestines; inside is the multiplication part.",
     "rating": 9.2,
-    "director": "$1.20",
+    "director": "$1.00",
     "year": '<img src="img/instagram/banhmithit.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -45,7 +45,7 @@ var movies = [
     "name": 'BANH CUON <br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Roll cake also known as cake or wet cake is the name of a food made from steamed dough coated thinly, curled up, inside a vegetable or meat filling.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.02",
     "year": '<img src="img/instagram/banhcuon.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -66,7 +66,7 @@ var movies = [
     "name": 'MI QUANG<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Mi Quang is usually made from fine ground rice flour mixed with flaxseed water and a golden star egg and then coated in thin layers, sliced horizontally to create a thin noodle about 5-10mm.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.05",
     "year": '<img src="img/instagram/miquang.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -87,7 +87,7 @@ var movies = [
     "name": 'COM TAM<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Com Tam is a Vietnamese dish made from rice with broken rice grains. Broken rice refers to broken rice grains, and rice refers to cooked rice.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.06",
     "year": '<img src="img/instagram/comtam.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -108,7 +108,7 @@ var movies = [
     "name": 'SUSHI<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Sushi is a Japanese dish of rice mixed with vinegar (shari) combined with other ingredients (neta).",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.09",
     "year": '<img src="img/instagram/shushi.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -131,7 +131,7 @@ var movies = [
     "name": 'COM CHIEN HAI SAN<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Fried rice is a favorite breakfast dish of many people. A simple way to make burnt rice, just rice, cooking oil and seasoning is enough.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.10",
     "year": '<img src="img/instagram/comchienhaisan.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -151,7 +151,7 @@ var movies = [
     "name": 'CANH CU SEN<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Just eating or drinking lotus root water will help you sleep better and deeper.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.15",
     "year": '<img src="img/instagram/canhcusen.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -174,7 +174,7 @@ var movies = [
     "name": 'GA SOT CAY<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Spicy chicken dish is easy to make, attractive when eaten with white rice thanks to the sour, spicy taste from ketchup and chili sauce.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.20",
     "year": '<img src="img/instagram/gasotcay.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -195,7 +195,7 @@ var movies = [
     "name": 'BO BIT TET<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Beef steak is a famous European dish favored by Vietnamese people, I don't know when it was introduced to Vietnam, but now it's too popular.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.30",
     "year": '<img src="img/instagram/bobittet.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -215,7 +215,7 @@ var movies = [
     "name": 'SPAGHETTI<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Spaghetti, more commonly known as pasta, is a long, small, cylindrical type of pasta. It is an essential food in traditional Italian cuisine.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.50",
     "year": '<img src="img/instagram/spaghetti.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
@@ -237,7 +237,7 @@ var movies = [
     "name": 'PIZZA<br/><i class="fas fa-shopping-cart" style="color: black;"></i><a href="shop.html" style="color: black;">Go to order</a>',
     "outline": "Pizza, which Vietnamese people often read as, is a flat, round cake made from water, flour and yeast, after being incubated for at least 24 hours and molded into a round and flat cake. and is put in the oven to cook.",
     "rating": 9.1,
-    "director": "$1.21",
+    "director": "$1.80",
     "year": '<img src="img/instagram/pizza.jpg" style="height: 100%; width: 100%;">',
     "stars": [
       "Món mặn",
